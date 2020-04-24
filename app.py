6ae81b5d7961c8314711753754c69c849b2fc2e4
Stefan Robu <stefanadrianrobu@yahoo.com>
2020-04-24 18:37:44 +0200

    Implemented the file uploader
    This data uploader works with csv files, displays them wierdly, will work on it and update it when possible

--- app.py
+++ app.py
@@ -1,23 +1,46 @@
-from  flask import Flask,render_template,url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import os
+from  flask import Flask,flash,render_template,request,redirect,url_for,send_from_directory
+from werkzeug.utils import secure_filename
+import pandas as pd
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'csv'}
+
+
+
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqllite:///'
-db=SQLAlchemy(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-class Todo(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    content=db.Column(db.String(200),nullable=False)
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/',methods=['GET','POST'])
+def upload_file():
+    if request.method== 'POST':
+        #checks if the post has the file part
+        if 'file' not in request.files:
+            flash('no file part')
+            return redirect (request.url)
+        file =request.files['file']
+        #if user does not select a file, browser also
+        # submit an empty part with no filename
+        if file.filename== '':
+            flash('no selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename=secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            return redirect(url_for('uploaded_file',
+                                      filename=filename))                          
+    return render_template("index.html")                                    
     
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-@app.route('/')
-def index():
-    return render_template("index.html")
-
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    data=pd.read_csv(filename,encoding = "cp1252")
+    return render_template("table.html",name=filename,data=data)
 if __name__=="__main__":
     app.run(debug=True)
 
