37d7561c6d25a57125d5f1fdf24ed4c6b5449736
Stefan Robu <stefanadrianrobu@yahoo.com>
2020-05-27 23:05:15 +0200

    Images are now somewhat supported on the file uploader

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import os
 import app
-from  flask import Flask,flash,render_template,request,redirect,url_for,send_from_directory,session
+from  flask import Flask, flash,render_template,request,redirect,url_for
 from werkzeug.utils import secure_filename
 import pandas as pd
 import random
 import numpy as np
 from PIL import Image
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, curdoc
 from bokeh.palettes import turbo
 from bokeh.embed import components
 
-UPLOAD_FOLDER = './uploads' #MAKE SURE TO CREATE A FOLDER FOR THIS IN THE CODE FOLDER
+
 ALLOWED_EXTENSIONS = {'csv','jpg', 'jpeg'}
 
 app=Flask(__name__)
 app.secret_key = "key"
 #configuring the upload folder and the maximum size
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 #defines the allowed file types
 
@@ -24,12 +24,14 @@ stimuli=""
 stimuli_url=""
 data_url=""
 
+UPLOAD_FOLDER=os.path.join(app.root_path,'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/',methods=['GET','POST']) #http methods, GET is managing information not secure, POST is Secure
-def initial_upload_file():
+def csv_upload_file():
 
     if request.method== 'POST': #if the transfer is Secure
         #checks if the post has the file
@@ -45,13 +47,13 @@ def initial_upload_file():
         if file and allowed_file(file.filename):
                 dataset=secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'],dataset))
-                data_url = os.path.join(app.config['UPLOAD_FOLDER'],dataset)
-                return redirect(url_for('csv_file',data_url=data_url))
+                
+                return redirect(url_for('csv_file',dataset=dataset))
 
     return render_template("index.html")     
 
-@app.route('/uploads/<data_url>',methods=['GET','POST'])
-def csv_file(data_url): #file uploaded is a csv file, and image needs to be uploaded
+@app.route('/upload_csv/<dataset>',methods=['GET','POST'])
+def csv_file(dataset): #file uploaded is a csv file, and image needs to be uploaded
     if request.method== 'POST': #if the transfer is Secure
         #checks if the post has the file
         if 'file' not in request.files:
@@ -67,28 +69,25 @@ def csv_file(data_url): #file uploaded is a csv file, and image needs to be uplo
             stimuli=secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],stimuli))
             
-            return redirect(url_for('gazeplot_generate',stimuli=stimuli,data_url=data_url))    
+            return redirect(url_for('gazeplot_generate',stimuli=stimuli,dataset=dataset))    
     return render_template("upload_image.html")   
 
-@app.route('/image_generate/<data_url>/<stimuli>/')
-def gazeplot_generate(stimuli,data_url):
-    stimuli_url = os.path.join(app.config['UPLOAD_FOLDER'],stimuli)
+@app.route('/graph_generate/<dataset>/<stimuli>/')
+def gazeplot_generate(stimuli,dataset):
+    #generating the paths
+    data_url = os.path.join(app.config['UPLOAD_FOLDER'],dataset)
+    stimuli_path = os.path.join(app.config['UPLOAD_FOLDER'],stimuli)
+
     data=pd.read_csv(data_url,encoding = "latin1",delim_whitespace=True)
     stimuli_filter=data['StimuliName']==stimuli
     mapped=data[stimuli_filter]
-
     user_array=mapped['user'].unique()
-
     palette = turbo(256)
-
-    img =  Image.open(stimuli_url)
+    img =  Image.open(stimuli_path)
     width, height = img.size
-
-    directory = os.path.dirname(os.path.realpath(__file__))[2:]
-    newPath = directory.replace(os.sep, '/') + '/' + stimuli
-
-    plot = figure(plot_width =800 , plot_height=700, x_range=(0,width), y_range=(0,height))
-    plot.image_url(url=[newPath], x=0, y=0, h=height, w=width, alpha=1)
+    img_url='http://127.0.0.1:5000/uploads/'+stimuli+'/' #need to change the IP stuff based on your IP
+    plot = figure(plot_width =800 , plot_height=700, x_range=(0,width), y_range=(height,0))
+    plot.image_url(url=[img_url], x=0, y=0, h=height, w=width, alpha=1)
 
     j=0
     specific_color = []
@@ -102,8 +101,7 @@ def gazeplot_generate(stimuli,data_url):
         points=mapped[mapped['user']==user].sort_values(by='Timestamp')
         plot.line(points['MappedFixationPointX'], points['MappedFixationPointY'], line_width=2, alpha=0.65, color=color)
         plot.circle(points['MappedFixationPointX'], points['MappedFixationPointY'],size=(points['FixationDuration']/25), color=points['color'], alpha=0.85)
-
     script, div = components(plot,wrap_script=False)
-    return render_template('layout.html', plot_script=script, plot_div=div)
+    return render_template('layout.html',gaze_script=script,gaze_div=div)
 if __name__=="__main__":
     app.run(debug=True)
