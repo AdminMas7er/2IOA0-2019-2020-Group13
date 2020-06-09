ad2ac27281cd5f33aa0846ef83425f3399a7662a
Noortje van Velzen <noortje.vanvelzen@hotmail.com>
2020-06-09 14:02:36 +0200

    Eye Clouds (90%)

--- Visualizations/eyecloud.py
+++ Visualizations/eyecloud.py
@@ -1,3 +1,6 @@
+# Eye Clouds Visualization
+
+# Imported modules
 import numpy as np
 import pandas as pd
 import random
@@ -10,10 +13,10 @@ from sklearn.cluster import KMeans
 
 output_file("image.html", mode='inline')
 
-data = pd.read_csv('C:/Users/20190825/Desktop/TUE NOTES/2IOA0/MetroMapsEyeTracking/MetroMapsEyeTracking/all_fixation_data_cleaned_up.csv', encoding='latin1', delim_whitespace = True)
+data = pd.read_csv('C:/Users/20184080/Downloads/MetroMapsEyeTracking/MetroMapsEyeTracking/all_fixation_data_cleaned_up.csv', encoding='latin1', delim_whitespace = True)
 
-stimuli = '10_Barcelona_S1.jpg'
-stimuli_url = 'C:/Users/20190825/Desktop/TUE NOTES/2IOA0/MetroMapsEyeTracking/MetroMapsEyeTracking/stimuli/10_Barcelona_S1.jpg'
+stimuli = '01_Antwerpen_S1.jpg'
+stimuli_url = 'C:/Users/20184080/Downloads/MetroMapsEyeTracking/MetroMapsEyeTracking/stimuli/01_Antwerpen_S1.jpg'
 
 stimuli_filter=data['StimuliName']==stimuli
 mapped=data[stimuli_filter]
@@ -23,7 +26,7 @@ img_rgb = img.convert('RGB') #converting the image in RGB values
 
 Cluster_map=mapped[['MappedFixationPointX','MappedFixationPointY','FixationDuration']].copy() #creating a separate dataframe for the clustering
 
-km=KMeans(n_clusters=6) #number of clusters
+km=KMeans(n_clusters=7) #number of clusters
 km.fit(Cluster_map)
 centers=pd.DataFrame(km.cluster_centers_,columns=Cluster_map.columns) #generating the centre of each cluster
 centers_coords=centers[['MappedFixationPointX','MappedFixationPointY','FixationDuration']].itertuples(index=False,name=None)  #putting the x, y, size values of each centre in a tuple
@@ -43,17 +46,44 @@ for x,y,size in centre_pairs: #generating the cricle crop by creating a separate
 centers['thumbnails']=cropped_thumbs
 
 centers['FixationDuration']=centers['FixationDuration']/6.25 #resizing each image
-ds=ColumnDataSource(centers)
+
+sorted_centers_raw = centers.sort_values(by = 'FixationDuration', ascending = False)
+sorted_centers = sorted_centers_raw.reset_index()
+
+circle_1d = sorted_centers['FixationDuration'][0]
+circle_2d = sorted_centers['FixationDuration'][1]
+circle_3d = sorted_centers['FixationDuration'][2]
+circle_4d = sorted_centers['FixationDuration'][3]
+circle_5d = sorted_centers['FixationDuration'][4]
+circle_6d = sorted_centers['FixationDuration'][5]
+circle_7d = sorted_centers['FixationDuration'][6]
+
+sorted_centers['MappedFixationPointX'][0] = 500
+sorted_centers['MappedFixationPointY'][0] = 500
+sorted_centers['MappedFixationPointX'][1] = 500 + (0.5 * circle_1d) - (0.5 * circle_2d)
+sorted_centers['MappedFixationPointY'][1] = 500 + circle_1d
+sorted_centers['MappedFixationPointX'][2] = 500 + ((14/15) * circle_1d)
+sorted_centers['MappedFixationPointY'][2] = 500 + ((2/3) * circle_1d)
+sorted_centers['MappedFixationPointX'][3] = 500 + ((14/15) * circle_1d)
+sorted_centers['MappedFixationPointY'][3] = 500 + ((1/3) * circle_1d) - circle_4d
+sorted_centers['MappedFixationPointX'][4] = 500 + (0.5 * circle_1d) - (0.5 * circle_5d)
+sorted_centers['MappedFixationPointY'][4] = 500 - circle_5d
+sorted_centers['MappedFixationPointX'][5] = 500 - ((1.5/10) * circle_1d)
+sorted_centers['MappedFixationPointY'][5] = 500 + ((1/3) * circle_1d) - circle_6d
+sorted_centers['MappedFixationPointX'][6] = 500 - ((1.5/10) * circle_1d)
+sorted_centers['MappedFixationPointY'][6] = 500 + ((2/3) * circle_1d) 
+
+print(sorted_centers)
+
+ds = ColumnDataSource(sorted_centers)
+
 plot_eyeclouds = figure(plot_width =1000 , plot_height=700, match_aspect=True)
 plot_eyeclouds.xgrid.visible = False
 plot_eyeclouds.ygrid.visible = False
 plot_eyeclouds.xaxis.visible = False
 plot_eyeclouds.xaxis.visible = False
+plot_eyeclouds.background_fill_color = 'black'
+plot_eyeclouds.background_fill_alpha = 0.8
 plot_eyeclouds.image_rgba(image='thumbnails', x='MappedFixationPointX', y='MappedFixationPointY', dw='FixationDuration', dh='FixationDuration', source=ds) #trying to draw the points
-
-
-
-
-
 
 show(plot_eyeclouds)
