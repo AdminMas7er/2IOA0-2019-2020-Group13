b7c2e3b2c8c369309957cc255a690bfc5b976be4
Noortje van Velzen <noortje.vanvelzen@hotmail.com>
2020-06-10 15:24:36 +0200

    Interaction Tools Eye Clouds (99%)
    Lasso, Undo, Redo, ZoomIn, ZoomOut, Hover

--- Visualizations/eyecloud.py
+++ Visualizations/eyecloud.py
@@ -1,12 +1,13 @@
 # Eye Clouds Visualization
 
-# Imported modules
+# Importing standard Python Libraries
 import numpy as np
 import pandas as pd
 import random
 
+# Importing specific modules
 from PIL import Image, ImageDraw
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, LassoSelectTool, UndoTool, RedoTool, HoverTool, ZoomInTool, ZoomOutTool
 from bokeh.palettes import turbo
 from bokeh.plotting import figure, output_file, show
 from sklearn.cluster import KMeans
@@ -47,9 +48,11 @@ centers['thumbnails']=cropped_thumbs
 
 centers['FixationDuration']=centers['FixationDuration']/6.25 #resizing each image
 
+# Sorting the dataset based from high Fixation Duration to low Fixation Duration
 sorted_centers_raw = centers.sort_values(by = 'FixationDuration', ascending = False)
 sorted_centers = sorted_centers_raw.reset_index()
 
+# Determining diameter of each circle
 circle_1d = sorted_centers['FixationDuration'][0]
 circle_2d = sorted_centers['FixationDuration'][1]
 circle_3d = sorted_centers['FixationDuration'][2]
@@ -58,6 +61,11 @@ circle_5d = sorted_centers['FixationDuration'][4]
 circle_6d = sorted_centers['FixationDuration'][5]
 circle_7d = sorted_centers['FixationDuration'][6]
 
+# Saving X and Y coordinates for Hovertool under new names
+sorted_centers['OriginalMappedFixationPointX'] = sorted_centers['MappedFixationPointX']
+sorted_centers['OriginalMappedFixationPointY'] = sorted_centers['MappedFixationPointY']
+
+# Reassigning values X and Y coordinates for reformation of the circles
 sorted_centers['MappedFixationPointX'][0] = 500
 sorted_centers['MappedFixationPointY'][0] = 500
 sorted_centers['MappedFixationPointX'][1] = 500 + (0.5 * circle_1d) - (0.5 * circle_2d)
@@ -72,18 +80,36 @@ sorted_centers['MappedFixationPointX'][5] = 500 - ((1.5/10) * circle_1d)
 sorted_centers['MappedFixationPointY'][5] = 500 + ((1/3) * circle_1d) - circle_6d
 sorted_centers['MappedFixationPointX'][6] = 500 - ((1.5/10) * circle_1d)
 sorted_centers['MappedFixationPointY'][6] = 500 + ((2/3) * circle_1d) 
-
 print(sorted_centers)
 
+# Maps names of columns to arrays
 ds = ColumnDataSource(sorted_centers)
 
+## Creating the Eye Clouds visualization 
+
+# Creating the plot
 plot_eyeclouds = figure(plot_width =1000 , plot_height=700, match_aspect=True)
+
+# Adjusting the grid
 plot_eyeclouds.xgrid.visible = False
 plot_eyeclouds.ygrid.visible = False
 plot_eyeclouds.xaxis.visible = False
 plot_eyeclouds.xaxis.visible = False
+
+# Adjusting the background
 plot_eyeclouds.background_fill_color = 'turquoise'
 plot_eyeclouds.background_fill_alpha = 0.2
+
+# Adding Tools 
+plot_eyeclouds.add_tools(LassoSelectTool())
+plot_eyeclouds.add_tools(UndoTool())
+plot_eyeclouds.add_tools(RedoTool())
+plot_eyeclouds.add_tools(ZoomInTool())
+plot_eyeclouds.add_tools(ZoomOutTool())
+plot_eyeclouds.add_tools(HoverTool(tooltips=[('X-Coordinate of Fixation', '@OriginalMappedFixationPointX'), ('Y-Coordinate of Fixation', '@OriginalMappedFixationPointY'), ('Total Duration of Fixation', '@FixationDuration')]))
+
+# Creating RGB images inside the plot
 plot_eyeclouds.image_rgba(image='thumbnails', x='MappedFixationPointX', y='MappedFixationPointY', dw='FixationDuration', dh='FixationDuration', source=ds) #trying to draw the points
 
+# Call the plot
 show(plot_eyeclouds)
